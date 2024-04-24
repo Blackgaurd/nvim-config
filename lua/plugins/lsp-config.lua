f25565237096981f91974f7aaedf4094c15c6382
Bryan Deng <bryan.deng002@gmail.com>
2024-04-24 11:09:19 -0400

    add neogit

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,19 +12,20 @@ return {
         config = function()
             require("mason-lspconfig").setup({
                 ensure_installed = {
-                    "bashls",    -- bash
-                    "clangd",    -- c/c++
-                    "cssls",     -- css
-                    "gopls",     -- go
-                    "html",      -- html
-                    "ltex",      -- latex
-                    "lua_ls",    -- lua
-                    "marksman",  -- markdown
-                    "ocamllsp",  -- ocaml
-                    "ruff",      -- python
-                    "svelte",    -- svelte
-                    "tsserver",  -- js/ts
-                    "typst_lsp", -- typst
+                    "bashls",        -- bash
+                    "clangd",        -- c/c++
+                    "cssls",         -- css
+                    "gopls",         -- go
+                    "html",          -- html
+                    "ltex",          -- latex
+                    "lua_ls",        -- lua
+                    "marksman",      -- markdown
+                    "ocamllsp",      -- ocaml
+                    "ruff",          -- python
+                    "rust_analyzer", -- rust
+                    "svelte",        -- svelte
+                    "tsserver",      -- js/ts
+                    "typst_lsp",     -- typst
                 }
             })
         end
@@ -46,6 +47,7 @@ return {
             lspconfig.marksman.setup({ capabilities = capabilities })
             lspconfig.ocamllsp.setup({ capabilities = capabilities })
             lspconfig.ruff.setup({ capabilities = capabilities })
+            lspconfig.rust_analyzer.setup({ capabilities = capabilities })
             lspconfig.svelte.setup({ capabilities = capabilities })
             lspconfig.tsserver.setup({ capabilities = capabilities })
             lspconfig.typst_lsp.setup({ capabilities = capabilities })
